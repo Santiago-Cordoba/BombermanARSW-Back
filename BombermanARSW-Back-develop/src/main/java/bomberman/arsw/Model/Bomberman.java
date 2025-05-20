3c1c664e8503bd8d43fab0b8f8524cbad9bc2bf5
Daniel Alejandro AV☄️ <89206637+AlejoCNYT@users.noreply.github.com>
2025-05-19 20:54:53 -0500

    Delete BombermanARSW-Back-develop/src directory

--- BombermanARSW-Back-develop/src/main/java/bomberman/arsw/Model/Bomberman.java
+++
@@ -1,9 +0,0 @@
-package bomberman.arsw.Model;
-
-import java.util.List;
-
-public class Bomberman {
-    private List<Player> players;
-    private GameMap gameMap;
-    private boolean gameOver;
-}
