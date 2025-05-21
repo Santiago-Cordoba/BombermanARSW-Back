4feac44599dc4d0c43328254d15ee209f0fcf09a
Santiago-Cordoba <santiagocordobaduenas@gmail.com>
2025-05-21 16:23:38 -0500

    no funciona nada yuju

--- arsw/src/main/java/bomberman/arsw/config/RedisConfig.java
+++ arsw/src/main/java/bomberman/arsw/config/RedisConfig.java
@@ -1,23 +1,68 @@
 package bomberman.arsw.config;
 
 import bomberman.arsw.Model.GameBoard;
-import bomberman.arsw.Model.Player;
 import bomberman.arsw.Model.Room;
+import io.lettuce.core.ClientOptions;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+import java.time.Duration;
+
 @Configuration
 public class RedisConfig {
+
+    @Value("${spring.data.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.data.redis.port}")
+    private int redisPort;
+
+    @Value("${spring.data.redis.password}")
+    private String redisPassword;
+
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+        config.setHostName(redisHost);
+        config.setPort(redisPort);
+        config.setPassword(redisPassword);
+
+        // Configurar ClientOptions primero
+        ClientOptions options = ClientOptions.builder()
+                .autoReconnect(true)
+                .cancelCommandsOnReconnectFailure(true)
+                .disconnectedBehavior(ClientOptions.DisconnectedBehavior.REJECT_COMMANDS)
+                .build();
+
+        // Configurar LettuceClientConfiguration
+        LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
+                .commandTimeout(Duration.ofSeconds(5))
+                .shutdownTimeout(Duration.ZERO)
+                .clientOptions(options)  // Usar las opciones configuradas
+                .useSsl()  // Habilitar SSL al final
+                .build();
+
+        LettuceConnectionFactory factory = new LettuceConnectionFactory(config, clientConfig);
+        factory.setValidateConnection(true);
+        return factory;
+    }
+
     @Bean
     public RedisTemplate<String, GameBoard> gameBoardRedisTemplate(RedisConnectionFactory connectionFactory) {
         RedisTemplate<String, GameBoard> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
         template.setKeySerializer(new StringRedisSerializer());
         template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        template.setEnableTransactionSupport(true);
+        template.afterPropertiesSet();
         return template;
     }
 
@@ -27,6 +72,8 @@ public class RedisConfig {
         template.setConnectionFactory(connectionFactory);
         template.setKeySerializer(new StringRedisSerializer());
         template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        template.setEnableTransactionSupport(true);
+        template.afterPropertiesSet();
         return template;
     }
 }
